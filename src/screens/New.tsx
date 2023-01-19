568942c2df80b46852d286d6c052f75600d56710
marlisonmourao <bentesmourao@gmail.com>
2023-01-19 03:30:35 -0400

    confirm button

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
-import { BackButton } from "../components/BackButton";
-import { CheckBox } from "../components/CheckBox";
+import { useState } from 'react';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { BackButton } from '../components/BackButton';
+import { CheckBox } from '../components/CheckBox';
+import { Feather } from '@expo/vector-icons'
+
+import colors from 'tailwindcss/colors';
 
 const avaiableWeekDays = [
   "Domingo",
@@ -25,7 +28,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -36,7 +42,11 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <TextInput 
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" 
+          placeholder="Exercícios, dormir bem, etc.."
+          placeholderTextColor={colors.zinc[400]}
+        />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -50,6 +60,21 @@ export function New() {
             onPress={() => handleToggleWeekDay(index)}
           />
         ))}
+
+        <TouchableOpacity 
+          activeOpacity={0.7}
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+        >
+          <Feather 
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
