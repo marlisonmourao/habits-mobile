bc5a83b793dc0e199fe46c5c90055612b9b3f9ca
marlisonmourao <bentesmourao@gmail.com>
2023-01-28 19:02:55 -0400

    local notifications

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import './src/lib/dayjs'
 
-import { StatusBar } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+import { StatusBar, Button } from 'react-native';
 import { 
   useFonts, 
   Inter_400Regular, 
@@ -13,6 +15,14 @@ import { Loading } from './src/components/Loading';
 
 import { Routes } from './src/routes';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  })
+})
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular, 
@@ -20,6 +30,24 @@ export default function App() {
     Inter_700Bold, 
     Inter_800ExtraBold 
   })
+
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now())
+    trigger.setMinutes(trigger.getMinutes() + 1)
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Olá, Marlison! 😎',
+        body: 'Você praticou seus hábitos hoje?'
+      },
+      trigger
+    })
+  }
+
+  async function getScheduleNotification() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync()
+    console.log(schedule)
+  }
 
   if(!fontsLoaded) {
     return <Loading />
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://10.0.0.107:3333'
+  baseURL: 'http://192.168.0.38:3333'
 })
